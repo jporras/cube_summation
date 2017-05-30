33f7941c8c0b29b92e30edcc7c2d44192511818c
Jorge Porras <jorgeporras326@gmail.com>
2017-05-30 14:29:54 -0500

    Crear vista

---
+++ resources/views/cube_summation.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Cube Summation</title>
+        <style></style>
+    </head>
+    <body>
+            <div class="content">
+                <div class="title">Laravel</div>
+            </div>
+        </div>
+    </body>
+</html>
